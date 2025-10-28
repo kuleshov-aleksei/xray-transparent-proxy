48517946e0dd4eb77e5d3fa1be5525cf452ba976
kuleshov-aleksei <alexxey67@gmail.com>
2025-10-28 15:35:16 +0300

    Add backup config

--- 11_install_service.sh
+++ 11_install_service.sh
@@ -5,8 +5,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-cp xray-configs/v2ray-extended.json /usr/local/etc/xray/v2ray-extended.json
+#cp xray-configs/v2ray-extended.json /usr/local/etc/xray/v2ray-extended.json
+cp xray-configs/v2ray-de.encamy.com.json /usr/local/etc/xray/v2ray-extended.json
 cp service-configs/90-custom-override-xray.conf /etc/systemd/system/xray.service.d/
 systemctl daemon-reload
+systemctl restart xray.service
 
 cp service-configs/10_sudoers_toggle /etc/sudoers.d/10_tproxy_toggle
