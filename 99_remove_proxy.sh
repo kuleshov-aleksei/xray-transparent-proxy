eecaf713081f8b0605905f3f87153ac07d1eee2b
kuleshov-aleksei <alexxey67@gmail.com>
2026-06-15 22:31:54 +0300

    Update proxy scripts

--- 99_remove_proxy.sh
+++ 99_remove_proxy.sh
@@ -7,3 +7,4 @@ unset HTTPS_PROXY
 unset no_proxy
 unset NO_PROXY
 unset all_proxy
+unset proxy
--- 99_set_proxy.sh
+++ 99_set_proxy.sh
@@ -4,9 +4,12 @@ PORT=1081
 
 export HTTPS_PROXY=http://127.0.0.1:$PORT
 export HTTP_PROXY=http://127.0.0.1:$PORT
-export NO_PROXY=localhost,127.0.0.1
+export NO_PROXY=localhost,127.0.0.1,.local,gitverse.ru,npm-mirror.gitverse.ru
 
 export http_proxy=http://127.0.0.1:$PORT
 export https_proxy=http://127.0.0.1:$PORT
 export all_proxy=http://127.0.0.1:$PORT
-export no_proxy=localhost,127.0.0.1
+export no_proxy=localhost,127.0.0.1,.local,gitverse.ru,npm-mirror.gitverse.ru
+
+export proxy=http://127.0.0.1:$PORT
+
